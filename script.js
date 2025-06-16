8cdd5334c39fed72fd148a3f91df6c423e9e8830
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-06-17 01:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+// 
+
+ // Select the button and the paragraph
+    const button = document.getElementById("enterBtn");
+    const statusParagraph = document.getElementById("status");
+
+    // Add event listener for click
+    button.addEventListener("click", function () {
+      statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+    });
